17a9e991b235f6a64a467645f4a56cf7e8b686f7
limzzum <chfhddl1234@naver.com>
2022-11-06 09:58:21 +0900

    feat : P2309 일곱난쟁이

---
+++ test/P2309Test.java
@@ -0,0 +1,16 @@
+import P2309.P2309;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class P2309Test {
+    @Test
+    @DisplayName("입력값이 100을 넘지 않는다")
+    void inputValueIsUnder100() throws IOException {
+        P2309.inputRead();
+
+
+}
+
+}
