bf094dda39805ffad7a02b14bd6b4314fe7aa3f5
limzzum <chfhddl1234@naver.com>
2022-12-09 17:34:18 +0900

    feat : P11279 최대 힙 java 풀이

---
+++ src/silver2/P11279/P11279.java
@@ -0,0 +1,32 @@
+package silver2.P11279;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class P11279 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> qp = new PriorityQueue<>(Comparator.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0){
+                if(qp.isEmpty()){
+                    sb.append(0).append("\n");
+                }
+                else{
+                    sb.append(qp.poll()).append("\n");
+                }
+            }
+            else{
+                qp.add(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
