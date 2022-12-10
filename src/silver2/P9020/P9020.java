a91b6ffd41e12e39b1416b3db0dea73a7ba85f2a
limzzum <chfhddl1234@naver.com>
2022-12-11 00:46:23 +0900

    feat : P9020 골드바흐의 추측 java 풀이

---
+++ src/silver2/P9020/P9020.java
@@ -0,0 +1,41 @@
+package silver2.P9020;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P9020 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i<N; i++){
+            int num = Integer.parseInt(br.readLine());
+            int[] answer = findAnswer(num, num / 2);
+            sb.append(answer[0]+" "+answer[1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int[] findAnswer(int origin, int part) {
+        if(isDecimal(part) && isDecimal(origin - part)){
+            return new int[]{part, origin -part};
+        }
+        return findAnswer(origin,part-1);
+    }
+
+    private static boolean isDecimal(int num) {
+        if(num ==1){
+            return false;
+        }
+        if(num == 2){
+            return true;
+        }
+        for(int i=2; i<= Math.sqrt(num); i++){
+            if(num%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
