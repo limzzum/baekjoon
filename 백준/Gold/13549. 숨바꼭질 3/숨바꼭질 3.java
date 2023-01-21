f9d0d6ee0d71fad5c0731129c24cc0e2c8b01b31
limzzum <69624097+limzzum@users.noreply.github.com>
2023-01-21 21:37:21 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 26564 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    static int count;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<int[]> qp = new LinkedList<>();
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        if(N>=K){
+            count = N-K;
+            System.out.println(count);
+            return;
+        }
+        count = K-N;
+        qp.add(new int[]{N,0});
+        boolean [] visited = new boolean[100001];
+        while (!qp.isEmpty()){
+            int[] poll = qp.poll();
+            if(poll[0]>=0 && poll[0]<=100000 && !visited[poll[0]]){
+                visited[poll[0]]=true;
+                if(poll[0] >= K){
+                    count = Math.min(count, poll[1] + poll[0] - K);
+                }else{
+                     qp.add(new int[]{poll[0]*2, poll[1]});
+                     qp.add(new int[]{poll[0]-1, poll[1]+1});
+                     qp.add(new int[]{poll[0]+1, poll[1]+1});
+                }                
+            }
+        }
+        System.out.println(count);
+    }
+
+}
