e04a8f57f5883afa807a7f2d3c28e66617c5c1ed
limzzum <69624097+limzzum@users.noreply.github.com>
2023-01-29 20:14:30 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -2,27 +2,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int [][] cost = new int[N][3];
-        for (int i=0; i<N; i++){
-            String[] line = br.readLine().split(" ");
-            for(int j=0; j<3; j++){
-                cost[i][j] = Integer.parseInt(line[j]);
+        int [][] cost = new int[N+1][3];
+        for(int i=1; i<=N; i++){
+            String [] line = br.readLine().split(" ");
+            for(int j= 0; j<line.length; j++){
+                int a = (j+1)%3;
+                int b = (j+2)%3;
+                cost[i][j] = Math.min(cost[i-1][a], cost[i-1][b]) + Integer.parseInt(line[j]);
             }
         }
-
-        for (int i=1; i<N; i++){
-            for(int j=0; j<3; j++){
-                cost[i][j] = Math.min(cost[i-1][(j+1)%3], cost[i-1][(j+2)%3]) + cost[i][j];
-            }
-        }
-
-        int answer = Math.min(cost[N-1][0], Math.min(cost[N-1][1], cost[N-1][2]));
+        int answer = Math.min(cost[N][0], Math.min(cost[N][1], cost[N][2]));
         System.out.println(answer);
-
 
     }
 }
