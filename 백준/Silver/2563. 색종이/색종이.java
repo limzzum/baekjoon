37a4df5ad32ef8488271249fca8bb69a27089a32
limzzum <69624097+limzzum@users.noreply.github.com>
2023-02-15 23:09:04 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int [][] map = new int[100][100];
+        int count = 0;
+        for(int i=0; i<N; i++){
+            String[] input = br.readLine().split(" ");
+            int c = Integer.parseInt(input[0]);
+            int r = 90 - Integer.parseInt(input[1]);
+
+            for(int row = r; row<r+10; row++){
+                for(int col = c; col< c+10; col++){
+                    if (map[row][col] == 0) {
+                        count +=1;
+                        map[row][col] = 1;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
