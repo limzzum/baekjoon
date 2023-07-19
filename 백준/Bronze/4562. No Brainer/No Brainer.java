488c30389c265f071483e8da248830ab163c58a5
limzzum <69624097+limzzum@users.noreply.github.com>
2023-07-19 20:50:11 +0900

    [Bronze III] Title: No Brainer, Time: 76 ms, Memory: 11292 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(a<b){
+                System.out.println("NO BRAINS");
+            }else {
+                System.out.println("MMM BRAINS");
+            }
+        }
+    }
+}
