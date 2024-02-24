2fbf9e90732882db996fad241e7283cb9a82711b
limzzum <69624097+limzzum@users.noreply.github.com>
2024-02-24 23:52:06 +0900

    [Bronze V] Title: Serca, Time: 980 ms, Memory: 26160 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(" @@@   @@@ \n"
+                + "@   @ @   @\n"
+                + "@    @    @\n"
+                + "@         @\n"
+                + " @       @ \n"
+                + "  @     @  \n"
+                + "   @   @   \n"
+                + "    @ @    \n"
+                + "     @     ");
+        }
+
+    }
+
+}
