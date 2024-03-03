2e373cbe6688334bd60a7f97a51dd5426bd4779a
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-03 22:09:13 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
