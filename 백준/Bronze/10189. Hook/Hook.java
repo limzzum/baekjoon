c6424140adc177faa49de63dd4bc920dbf759ca8
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-03 21:58:28 +0900

    [Bronze V] Title: Hook, Time: 120 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
