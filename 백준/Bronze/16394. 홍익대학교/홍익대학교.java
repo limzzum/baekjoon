056e4c60c9ed1c98bdb39f256f754f5dd2e14c33
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-03 21:35:32 +0900

    [Bronze V] Title: 홍익대학교, Time: 112 ms, Memory: 12932 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(new InputStreamReader(System.in));
+        int ans = sc.nextInt()-1946;
+        System.out.println(ans);
+
+
+    }
+}
