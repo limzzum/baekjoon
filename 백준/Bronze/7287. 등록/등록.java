d56998fd8dfbd1a69b6bfc834c8130a7342ab47a
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-03 22:01:29 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11252 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("243\n"+"chfhddl0127");
+
+        bw.flush();
+        bw.close();
+    }
+}
