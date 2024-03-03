28c4de03d5ea9d9dbc0becb9d5a8e154d755ac2d
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-03 21:57:25 +0900

    [Bronze IV] Title: 평균 점수, Time: 76 ms, Memory: 11400 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += Math.max(40, Integer.parseInt(br.readLine()));
+        }
+        System.out.println(sum/5);
+
+    }
+}
