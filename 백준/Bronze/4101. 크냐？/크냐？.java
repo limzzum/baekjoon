644f4da3b48feeb3d9de475667b86b3ffec9d36e
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-03 22:07:07 +0900

    [Bronze V] Title: 크냐?, Time: 84 ms, Memory: 11688 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        while (true){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(a == 0 && b == 0){
+                break;
+            }
+            String result = a > b ? "Yes" : "No";
+            bw.write(result+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
