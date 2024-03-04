8430a2a9ac9d1f1ebf8bfe76d1547b20e3b231bd
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-04 18:15:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int [][] map;
+    static int [] row = {0,-1,0,1};;
+    static int [] col = {-1, 0, 1, 0};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<int[]> qp = new LinkedList<>();
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        for (int i = 0; i<N; i++){
+            String[] line = br.readLine().split("");
+            for(int j = 0; j<N; j++){
+                int num = Integer.parseInt(line[j]);
+                if(num == 1){
+                    map[i][j] = num;
+                    qp.add(new int[]{i,j});
+                }
+            }
+        }
+
+        List<Integer> answer = new ArrayList<>();
+        while(!qp.isEmpty()){
+            int count = 1;
+            int[] position = qp.poll();
+            if(map[position[0]][position[1]]== 1) {
+                map[position[0]][position[1]]=0;
+                count = dfs( count, position);
+                answer.add(count);
+            }
+        }
+
+        Collections.sort(answer);
+        System.out.println(answer.size());
+        for (Integer integer : answer) {
+            System.out.println(integer);
+        }
+    }
+
+    private static int dfs( int count, int[] position) {
+        for (int i=0; i<4; i++){
+            int r = position[0]+ row[i];
+            int c = position[1]+ col[i];
+            if(r>=0 && r< N && c>=0 && c< N){
+                if(map[r][c]==1){
+                    count += 1;
+                    map[r][c] = 0;
+                    count = dfs(count, new int[]{r,c});
+                }
+            }
+        }
+        return count;
+    }
+}
