7a3a3ed3e4476f5661439688677e26532b9b2eae
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-04 18:36:47 +0900

    [Silver V] Title: 날짜 계산, Time: 124 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static public void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int one = Integer.parseInt(input[0]);
+        int two = Integer.parseInt(input[1]);
+        int three = Integer.parseInt(input[2]);
+
+        int MaxOne = 15;
+        int MaxTwo = 28;
+        int MaxThree = 19;
+
+        int year = two;
+        while (true){
+            if(year % MaxOne == one % MaxOne && year % MaxTwo == two % MaxTwo && year % MaxThree == three % MaxThree){
+                break;
+            }
+            year+= MaxTwo;
+        }
+        System.out.println(year);
+    }
+}
