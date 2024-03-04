920882e16d0028938b8522965269c9ee9e9621d3
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-04 18:34:54 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 88 ms, Memory: 11936 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        int j = 1;
+        while (count <= b) {
+            for (int i = 1; i <= j; i++) {
+                sb.append(j + " ");
+                count++;
+            }
+            j++;
+        }
+        String s = sb.toString();
+        st = new StringTokenizer(s);
+        int sum = 0;
+        for (int i = 1; i < a; i++) {
+            st.nextToken();
+        }
+        for (int i = a; i <= b; i++) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+        System.out.print(sum);
+    }
+}
