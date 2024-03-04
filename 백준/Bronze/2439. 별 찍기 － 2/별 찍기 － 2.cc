f02b4853985ff24c4781863d583425d875988b83
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-04 18:19:25 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+	int N, i,j,k;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++) {
+		for (k = 1; k <= N - i; k++) {
+			printf(" ");
+		}
+		for (j = 1; j <= i; j++) {
+				printf("*");
+			}
+		
+		printf("\n");
+	}
+		return 0;
+	}
