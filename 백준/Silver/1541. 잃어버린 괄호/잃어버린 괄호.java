68f60f637551a9c73a2840de1786aacdc1de6a3f
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-04 18:31:37 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Pattern pt = Pattern.compile("[-+]");
+        Pattern compile = Pattern.compile("[^-+]+");
+
+        String input = br.readLine();
+        String[] numbers =input.split(String.valueOf(pt));
+        String[] operator = input.replaceAll(String.valueOf(compile), "").split("");
+        int idx = 1;
+        for (String op : operator) {
+            if(op.equals("-")){
+                break;
+            }
+            idx += 1;
+        }
+        int sum = 0;
+        for (int i = 0; i< numbers.length; i++) {
+            if(i>=idx){
+                sum -= Integer.parseInt(numbers[i]);
+                continue;
+            }
+            sum += Integer.parseInt(numbers[i]);
+
+        }
+        System.out.println(sum);
+    }
+}
