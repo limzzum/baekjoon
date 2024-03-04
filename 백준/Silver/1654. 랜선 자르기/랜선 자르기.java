eeff5c6c6d6ae901e5ac093b8dad76731ed03b74
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-04 18:29:47 +0900

    [Silver II] Title: 랜선 자르기, Time: 200 ms, Memory: 17120 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int target = Integer.parseInt(input[1]);
+
+        long[] lanson = new long[N];
+        for (int i=0; i<N; i++){
+             lanson[i] = Long.parseLong(br.readLine());
+        }
+
+        Arrays.sort(lanson);
+        long left = 1;
+        long right = lanson[lanson.length-1];
+        long mid;
+        long sum;
+
+        long answer = 0;
+        while(left<=right){
+            sum =0;
+            mid = (left+right) /2;
+            for(long lan:lanson){
+                sum += lan/mid;
+            }
+            if(sum>=target){
+                answer = mid;
+                left = mid +1;
+            }
+            if(sum<target){
+                right = mid -1;
+            }
+        }
+        System.out.println(answer);
+
+    }
+}
