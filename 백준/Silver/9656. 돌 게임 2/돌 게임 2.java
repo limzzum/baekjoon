d760a14e7d8ed5d51db94767ef136074d36cdbcb
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-19 23:25:25 +0900

    [Silver V] Title: 돌 게임 2, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    
+    private static final String SANGGEUN_WIN_MESSAGE = "SK";
+    private static final String CHANGYOUNG_WIN_MESSAGE = "CY";
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+        
+	    boolean [] dp = new boolean[N+3];
+	    dp[1] = false;
+	    dp[2] = true;
+	    dp[3] = false;
+	    
+	    for(int i=4; i<= N; i++){
+	        if(!dp[i-1] || !dp[i-3]){
+	            dp[i] = true;
+	        }
+	    }
+	    
+	    if(dp[N]){
+	        System.out.println(SANGGEUN_WIN_MESSAGE);
+	    }else{
+	        System.out.println(CHANGYOUNG_WIN_MESSAGE);
+	    }
+    }
+}
