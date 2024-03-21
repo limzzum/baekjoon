3120c5734e68fefe08dfa21870d5292a8a60c9ff
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-21 21:09:30 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	    int N = Integer.parseInt(br.readLine());
+	    
+	    StringBuilder sb = new StringBuilder();
+	    for(int i=N; i>=1; i--){
+	        for(int j=0; j<i; j++){
+	            sb.append("*");
+	        }
+	        sb.append("\n");
+	    }
+	    
+	    System.out.println(sb);
+	    
+	    
+	}
+
+}
