cf70f91a5c60f19eb6bfdab9113f5d479e7af428
limzzum <69624097+limzzum@users.noreply.github.com>
2024-03-24 23:31:18 +0900

    [Silver II] Title: 돌 게임 4, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    
+    private static final String SANGGEUN_WIN_MESSAGE = "SK";
+    private static final String CHANGYOUNG_WIN_MESSAGE = "CY";
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int N = Integer.parseInt(br.readLine());
+        
+	    boolean [] dp = new boolean[N+4];
+	    dp[1] = false;
+	    dp[2] = true;
+        dp[3] = false;
+        dp[4] = true;
+	    
+	    for(int i=5; i<= N; i++){
+	        if(!dp[i-1] || !dp[i-3] || !dp[i-4]){
+	            dp[i] = true;
+	        }
+	    }
+	    
+	    if(dp[N]){
+	        System.out.println(SANGGEUN_WIN_MESSAGE);
+	    }else{
+	        System.out.println(CHANGYOUNG_WIN_MESSAGE);
+	    }
+    }
+}
