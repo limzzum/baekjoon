341dc540058b986fdcb252f6e53dfd1624f53ba7
limzzum <69624097+limzzum@users.noreply.github.com>
2024-04-24 00:00:23 +0900

    [Silver V] Title: 피보나치 수 4, Time: 176 ms, Memory: 21416 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -1,34 +1,23 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.math.*;
 
-public class Main {
+public class Main{
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-
-		if (N == 0) {
-			bw.write("0\n");
-		} else if (N == 1) {
-			bw.write("1\n");
-		} else {
-			BigInteger[] dp = new BigInteger[N + 1];
-
-			dp[0] = BigInteger.ZERO;
-			dp[1] = BigInteger.ONE;
-			for (int i = 2; i <= N; i++) {
-				dp[i] = dp[i - 2].add(dp[i - 1]);
-			}
-
-			bw.write(dp[N] + "\n");
-		}
-		bw.flush();
-		bw.close();
-		br.close();
+    static int N;
+    static BigInteger[] dp;
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new BigInteger[N+2];
+        
+        dp[0] = new BigInteger("0");
+        dp[1] = new BigInteger("1");
+        
+        for(int i=2; i<= N; i++){
+            dp[i] = dp[i-1].add(dp[i-2]);
+        }
+        System.out.println(dp[N]);
+        
 	}
 }
