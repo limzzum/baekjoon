106400bfd746a4bf77d8bd05a68f46e307b7493a
limzzum <69624097+limzzum@users.noreply.github.com>
2024-04-25 09:31:06 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 128 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main{
+
+    static int N;
+    static BigInteger[] dp;
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new BigInteger[N+2];
+        
+        dp[0] = new BigInteger("0");
+        dp[1] = new BigInteger("1");
+        
+        for(int i=2; i<= N; i++){
+            dp[i] = dp[i-1].add(dp[i-2]);
+        }
+        System.out.println(dp[N]);        
+	}
+}
