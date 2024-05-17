7159cacbff3780e29777bbed1f8b642376a88ae8
limzzum <69624097+limzzum@users.noreply.github.com>
2024-05-17 23:14:49 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,38 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
+    
+    static int N;
+    static int cnt;
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        Stack<Character> stack = new Stack<>();
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        
+        char[] input;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            char[] line = br.readLine().toCharArray();
-            for (char c : line) {
-                if(c == '('){
-                    stack.add(c);
-                }else if(!stack.isEmpty()){
-                    stack.pop();
-                }else {
-                    stack.add('F');
-                    break;
-                }
-            }
-            if(stack.isEmpty()){
-                sb.append("YES").append("\n");
+        for(int i=0; i<N; i++){
+            input = br.readLine().toCharArray();
+            cnt = 0;
+            if(isVPS(input)){
+                sb.append("YES");
             }else{
-                sb.append("NO").append("\n");
-                stack.clear();
+                sb.append("NO");
             }
-
+            sb.append("\n");
         }
 
-        System.out.println(sb);
-    }
-
-
+	    bw.write(sb.toString());
+	    bw.flush();
+	    bw.close();
+	}
+	
+	private static boolean isVPS(char[] input){
+	    for(char i : input){
+	        if(i == '('){
+	            cnt += 1;
+	        }else if(cnt == 0){
+	            return false;
+	        }else {
+	            cnt -=1;
+	        }
+	    }
+	    return cnt == 0;
+	}
 }
