7b9a196ae93d741f0bce5dad0fcf5f8e7afe7ccd
limzzum <69624097+limzzum@users.noreply.github.com>
2024-09-05 09:51:07 +0900

    [Gold V] Title: 사과나무, Time: 340 ms, Memory: 23364 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.java
+++ 백준/Gold/20002. 사과나무/사과나무.java
@@ -1,41 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
-    static int[][] map;
-    static int[][] sumMap;
-    static int answer = -301;
-    public static void main(String[] args) throws Exception {
+    static int[][] sum;
+    static int answer = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        sumMap = new int[N+1][N+1];
+        sum = new int[N+1][N+1];
 
         StringTokenizer st;
-        for (int i = 0; i < N; i++) {
+        for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                answer = Math.max(answer, map[i][j]);
-                sumMap[i+1][j+1] = map[i][j] + sumMap[i][j+1] + sumMap[i+1][j] - sumMap[i][j];
+            for(int j=1; j<=N; j++){
+                sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + Integer.parseInt(st.nextToken());
             }
         }
 
-        int size = 2;
-        while (size <= N){
-            for (int i = size; i <= N; i++) {
-                for (int j = size; j <= N; j++) {
-                    answer = Math.max(answer, sumMap[i][j] - sumMap[i-size][j] - sumMap[i][j-size] + sumMap[i-size][j-size]);
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                int max = Math.min(N-i, N-j);
+                for(int k=0; k<=max; k++){
+                    answer = Math.max(answer, sum[i+k][j+k] - sum[i+k][j-1] - sum[i-1][j+k] + sum[i-1][j-1]);
                 }
             }
-            size += 1;
         }
         System.out.println(answer);
-
-
     }
 
 }
