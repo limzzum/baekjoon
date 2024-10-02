aa272c2ef8f85902451891a03f95daf82e4e7732
limzzum <69624097+limzzum@users.noreply.github.com>
2024-10-02 09:51:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 180 ms, Memory: 54096 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,39 +1,51 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int K;
-    static List<int[]> bags = new ArrayList<>();
 
-    public static void main(String[] args) throws Exception {
+    static class Bag {
+        int weight;
+        int value;
+        public Bag(int weight, int value){
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+
+    static int N, K;
+    static Queue<Bag> bags = new PriorityQueue<>(Comparator.comparing(bag -> bag.weight));
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp = new int[N+1][K+1];
 
-        String[] input = br.readLine().split(" ");
-        N = Integer.parseInt(input[0]);
-        K = Integer.parseInt(input[1]);
-        int[][] dp = new int[N+1][K+1];
-
-        for (int i = 0; i < N; i++) {
-            String[] line = br.readLine().split(" ");
-            int weight = Integer.parseInt(line[0]);
-            int value = Integer.parseInt(line[1]);
-
-            bags.add(new int[]{weight, value});
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            bags.add(new Bag(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
 
-        for (int i = 1; i <= N; i++){
-            for(int j=1; j <= K; j++){
-                dp[i][j]= dp[i-1][j];
-                if(bags.get(i-1)[0] > j){
+        Bag bag;
+        for(int i=1; i<=N; i++){
+            bag = bags.poll();
+            for(int j=0; j<=K; j++){
+                if(bag.weight > j){
+                    dp[i][j] = dp[i-1][j];
                     continue;
                 }
-                dp[i][j] = Math.max(dp[i][j], dp[i-1][j-bags.get(i-1)[0]]+bags.get(i-1)[1]);
+                dp[i][j] = Math.max(dp[i-1][j], bag.value + dp[i-1][j-bag.weight]);
             }
         }
+
         System.out.println(dp[N][K]);
+
     }
 
 }
