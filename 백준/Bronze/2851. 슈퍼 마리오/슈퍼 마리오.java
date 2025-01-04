7c095481e3539474720a5a539786616912b94087
limzzum <69624097+limzzum@users.noreply.github.com>
2025-01-05 00:02:32 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,24 +1,23 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n;
-        int sum = 0;
-        int answer = 0;
-        for (int i = 0; i < 10; i++) {
-            n = Integer.parseInt(br.readLine());
-            sum += n;
-            if(sum>=100){
-                if(sum - 100 <= 100-answer){
-                    answer = sum;
-                }
-                System.out.println(answer);
-                return;
-            }
-            answer = sum;
-        }
-        System.out.println(answer);
-    }
+public class Main{
+	
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int sum = 0;
+	    int diff = 100;
+	    int n;
+	    for(int i=0; i<10; i++){
+	        n = Integer.parseInt(br.readLine());
+	        if(Math.abs(100 - (sum + n)) < diff
+	            || Math.abs(100 - (sum + n)) == diff && n > 0){
+	                diff = Math.abs(100 - (sum + n));
+	                sum += n;
+	                continue;
+	        }
+	        break;
+	    }
+	    System.out.print(sum);
+	}
 }
