c991f7a88f4d8ec7d9964f2ab83e382db9f8f174
limzzum <69624097+limzzum@users.noreply.github.com>
2025-09-14 18:25:25 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.27 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    
+    static Map<String, Integer> termsInfo = new HashMap<>();
+    static int todayValue;
+    
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        todayValue = getDateValue(today);
+        
+        StringTokenizer st;
+        for(String term : terms){
+            st = new StringTokenizer(term);
+            termsInfo.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+        }
+        
+        String date;
+        String term;
+        List<Integer> answerIndexs = new ArrayList<>();
+        for(int i=0; i<privacies.length; i++){
+            st = new StringTokenizer(privacies[i]);
+            date = st.nextToken();
+            term = st.nextToken();
+            if(isOverDate(date, term)){
+                answerIndexs.add(i+1);
+            }
+        }
+        int[] answer = new int[answerIndexs.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = answerIndexs.get(i);
+        }
+        return answer;
+    }
+    
+    private int getDateValue(String date){
+        String[] ymdDate = date.split("\\.");
+        int value = Integer.parseInt(ymdDate[2]);
+        value += 28 * Integer.parseInt(ymdDate[1]);
+        value += 28 * 12 * Integer.parseInt(ymdDate[0]);
+        return value;
+    }
+    
+    private boolean isOverDate(String date, String term){
+        return todayValue >= getDateValue(date) + termsInfo.get(term) * 28;
+    }
+}
