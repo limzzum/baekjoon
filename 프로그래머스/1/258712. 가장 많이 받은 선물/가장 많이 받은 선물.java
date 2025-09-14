73900e6a59d63e4bbc0ac2f46b0ca9e95f98292e
limzzum <69624097+limzzum@users.noreply.github.com>
2025-09-14 17:27:24 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 22.82 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,44 +1,65 @@
 import java.util.*;
-import java.util.stream.*;
-import java.io.*;
 
 class Solution {
-
-    static int answer = 0;
     
-    static Map<String, Map<String, Integer>> map = new HashMap(); 
-    static Map<String, Integer> score = new HashMap(); 
-    static StringTokenizer st;
-
-    public int solution(String[] friends, String[] gifts) {
+    static class Friend {
+        String name;
+        int presentPoint;
+        Map<String, Integer> relationship;
         
-        for(String friend: friends){
-            map.put(friend, new HashMap());
-            for(String f: friends){
-                if(friend.equals(f)){
-                    continue;
-                }
-                map.get(friend).put(f, 0);
-            }
+        Friend(String name){
+            this.name = name;
+            relationship = new HashMap<>();
         }
         
+        public void receiveFrom(String name){
+            presentPoint -= 1;
+            relationship.put(name, relationship.getOrDefault(name, 0) -1);
+        }
+        
+        public void sentTo(String name){
+            presentPoint += 1;
+            relationship.put(name, relationship.getOrDefault(name, 0) +1);
+        }
+        
+        public int getPresentPoint(){
+            return presentPoint;
+        }
+        
+        public int getPresentCnt(){
+            return (int) kakaoFriends.stream().map(x -> x.name).filter(x -> 
+                relationship.getOrDefault(x,0) > 0 || 
+                    (relationship.getOrDefault(x, 0) == 0 && presentPoint > kakaoFriends.get(indexs.get(x)).getPresentPoint())
+            ).count();
+        }
+        
+    }
+    
+    static List<Friend> kakaoFriends = new ArrayList<>();
+    static Map<String, Integer> indexs = new HashMap<>();
+    
+    public int solution(String[] friends, String[] gifts){
+        
+        for(String friend : friends){
+            kakaoFriends.add(new Friend(friend));
+            indexs.put(friend, kakaoFriends.size()-1);
+        }
+        
+        StringTokenizer st;
+        String from;
+        String to;
         for(String gift : gifts){
             st = new StringTokenizer(gift);
-            String from = st.nextToken();
-            String to = st.nextToken();
-            
-            score.put(from, score.getOrDefault(from, 0) +1);
-            score.put(to, score.getOrDefault(to, 0) -1);
-            
-            map.get(from).put(to, map.get(from).get(to) + 1);
-            map.get(to).put(from, map.get(to).get(from) - 1);
-
+            from = st.nextToken();
+            to = st.nextToken();
+            kakaoFriends.get(indexs.get(from)).sentTo(to);
+            kakaoFriends.get(indexs.get(to)).receiveFrom(from);
         }
         
-        answer = map.entrySet().stream().mapToInt(entry -> {
-        return (int) entry.getValue().entrySet().stream().filter(e-> e.getValue() > 0 || (e.getValue() == 0 && score.getOrDefault(entry.getKey(),0) > score.getOrDefault(e.getKey(), 0))).count();
-    }).max().orElse(0);
-
+        int answer = 0;
+        for(Friend friend : kakaoFriends){
+            answer = Math.max(answer, friend.getPresentCnt());
+        }
         return answer;
     }
 }
